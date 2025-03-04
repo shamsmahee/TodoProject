ce5985effff7eb73d7941594db0b9477a76bd728
shamsmahee <shamsulmahee@outlook.com>
2025-03-05 02:01:03 +0600

    react icons install

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,22 @@
+import { FaTrash } from "react-icons/fa";
+
 import React from 'react';
 
-const Todo = () => {
+const Todo = (props) => {
+    const { title, desc } = props.send
     return (
         <div>
-
+            <article>
+                <div>
+                    <h3>{title}</h3>
+                    <p>{desc}</p>
+                </div>
+                <div>
+                    <button>
+                        <FaTrash />
+                    </button>
+                </div>
+            </article>
         </div>
     );
 };
