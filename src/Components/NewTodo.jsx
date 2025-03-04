be154cd14bf9ad901201bea282c25eeaf54b832b
shamsmahee <shamsulmahee@outlook.com>
2025-03-05 03:13:14 +0600

    data send and show in console

--- src/Components/NewTodo.jsx
+++ src/Components/NewTodo.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './newtodo.module.css'
 const NewTodo = () => {
+
+    const [todo, setTodo] = useState({ title: '', desc: "" })
+    const { title, desc } = todo;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(todo);
+
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        setTodo((oldTodo) => {
+            return { ...oldTodo, [name]: event.target.value }
+        })
+    }
+
     return (
         <div>
-            <form className={style.form}>
+            <form className={style.form} onSubmit={handleSubmit}>
                 <div className={style["form-field"]}>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id='title' name='title' />
+                    <input type="text" id='title' name='title' value={title} onChange={handleChange} />
                 </div>
                 <div className={style["form-field"]}>
                     <label htmlFor="desc">Description:</label>
-                    <textarea type="text" id='desc' name='desc' />
+                    <textarea type="text" id='desc' name='desc' value={desc} onChange={handleChange} />
                 </div>
                 <button type='submit'>Add Task</button>
             </form>
